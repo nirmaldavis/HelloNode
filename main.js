fcc1597e5fb6a7b317e01dc4cf63906ff63d809b
nirmaldavis <nirmal.davis@gmail.com>
2016-02-21 21:56:51 +0530

    Basic Http Server using Node Js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Hello , Welcome to world of Node...!!");
+
+var http = require("http");
+
+var httpServer = http.createServer(function(request,response){
+    
+    response.writeHead(200, {'Content-Type' : 'text/plain'});
+    
+    response.end('Hello World\n');
+});
+
+httpServer.listen(8082);
+
+console.log("Server running at http://127.0.0.1:8082");
